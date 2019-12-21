fa185c61829b88aeb76633815b44553f4f263bc9
Guilherme Gonçalves Quaglietta <Guilherme.quaglietta@live.com>
2019-12-21 19:50:07 -0300

    Created pages and components: Home, Shopping, Accounts. Added a basic top menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './pages/home/topbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ShoppingComponent } from './pages/shopping/shopping.component';
+import { AccountComponent } from './pages/account/account.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    HomeComponent,
+    UserComponent,
+    ShoppingComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'shopping', component: ShoppingComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'user', component: UserComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
